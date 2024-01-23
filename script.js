8c995f746868d45726b20aeaa08bb2e70faab131
Rafael Costa da Silva <contatorafael50@gmail.com>
2024-01-23 11:39:15 -0300

    fixed functions

--- script.js
+++ script.js
@@ -1,49 +1,62 @@
 let qtd = [0, 0, 0, 0, 0];
 
 function addItens1() {
-  qtd[0] += 1
+  qtd[0] += 1;
 }
 
 function removeItens1() {
-  qtd[0] -= 1
+  qtd[0] -= 1;
+  if (qtd[0] < 0) {
+    qtd[0] == 0;
+  }
 }
 
 
 function addItens2() {
-  qtd[1] += 1
+  qtd[1] += 1;
 }
 
 function removeItens2() {
-  qtd[1] -= 1
+  qtd[1] -= 1;
+  if (qtd[1] < 0) {
+    qtd[1] == 0;
+  }
 }
 
 
 function addItens3() {
-  qtd[2] += 1
+  qtd[2] += 1;
 }
 
 function removeItens3() {
-  qtd[2] -= 1
+  qtd[2] -= 1;
+  if (qtd[2] < 0) {
+    qtd[2] == 0;
+  }
 }
 
 
 function addItens4() {
-  qtd[3] += 1
+  qtd[3] += 1;
 }
 
 function removeItens4() {
-  qtd[3] -= 1
+  qtd[3] -= 1;
+  if (qtd[3] < 0) {
+    qtd[3] == 0;
+  }
 }
 
 
 function addItens5() {
-  qtd[4] += 1
+  qtd[4] += 1;
 }
 
 function removeItens5() {
-  qtd[4] -= 1
+  qtd[4] -= 1;
+  if (qtd[4] < 0){
+    qtd[4] == 0;
+  }
 }
 
-for (let i = 0; i < 5; i++) {
-  
-}
+
