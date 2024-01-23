72c92dd8d8e04896bf9b8f8578325a7d2a93a1a8
Rafael Costa da Silva <contatorafael50@gmail.com>
2024-01-22 21:30:54 -0300

    added new functions and new id's

---
+++ script.js
@@ -0,0 +1,49 @@
+let qtd = [0, 0, 0, 0, 0];
+
+function addItens1() {
+  qtd[0] += 1
+}
+
+function removeItens1() {
+  qtd[0] -= 1
+}
+
+
+function addItens2() {
+  qtd[1] += 1
+}
+
+function removeItens2() {
+  qtd[1] -= 1
+}
+
+
+function addItens3() {
+  qtd[2] += 1
+}
+
+function removeItens3() {
+  qtd[2] -= 1
+}
+
+
+function addItens4() {
+  qtd[3] += 1
+}
+
+function removeItens4() {
+  qtd[3] -= 1
+}
+
+
+function addItens5() {
+  qtd[4] += 1
+}
+
+function removeItens5() {
+  qtd[4] -= 1
+}
+
+for (let i = 0; i < 5; i++) {
+  
+}
